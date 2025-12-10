23bf406168040da5b44fed74563985a5704273da
David <davidsantanar01@gmail.com>
2025-12-10 00:14:40 -0400

    Implement game functionality and UI components
    - Added GameScreen for game logic and user interaction.
    - Created NumberContainer, PrimaryButton, and Title components for UI.
    - Updated StartGameScreen to handle number input and validation.
    - Introduced Colors constant for consistent theming.
    - Enhanced App.js to manage game state and navigation between screens.

--- components/ui/PrimaryButton.js
+++ components/ui/PrimaryButton.js
@@ -1,9 +1,7 @@
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import Colors from '../../constants/colors'
 
 function PrimaryButton({ onPress, children }) {
-  function pressHandler() {
-    onPress?.();
-  }
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
@@ -12,34 +10,34 @@ function PrimaryButton({ onPress, children }) {
             ? [styles.buttonInnerContainer, styles.pressed]
             : styles.buttonInnerContainer
         }
-        onPress={pressHandler}
-        android_ripple={{ color: "#640233" }}
+        onPress={onPress}
+        android_ripple={{ color: Colors.primary600 }}
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
     </View>
-  );
+  )
 }
 
-export default PrimaryButton;
+export default PrimaryButton
 
 const styles = StyleSheet.create({
   buttonOuterContainer: {
     borderRadius: 28,
     margin: 4,
-    overflow: "hidden",
+    overflow: 'hidden',
   },
   buttonInnerContainer: {
-    backgroundColor: "#72063c",
+    backgroundColor: Colors.primary500,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
   },
   buttonText: {
-    color: "white",
-    textAlign: "center",
+    color: 'white',
+    textAlign: 'center',
   },
   pressed: {
     opacity: 0.75,
   },
-});
+})
